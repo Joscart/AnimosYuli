42cc21d60aa6a4495f264fd27cac22920e8fc408
Jose Guallasamin <jguallasaminc@est.ups.edu.ec>
2025-10-28 22:18:29 -0500

    add base

--- animos/astro.config.mjs
+++ animos/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Joscart.github.io',
+  base: '/AnimosYuli/',
   build: {
     assets: 'assets', // optional, if customizing
   }
