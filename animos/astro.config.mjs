9997598e23d6ddd8817611771b85b53e96f5733a
Jose Guallasamin <jguallasaminc@est.ups.edu.ec>
2025-10-28 22:35:41 -0500

    fix deploy

--- animos/astro.config.mjs
+++ animos/astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Joscart.github.io',
-  base: '/AnimosYuli/',
-  build: {
-    assets: 'assets', // optional, if customizing
-  }
+    site: 'https://Joscart.github.io',
+    base: '/AnimosYuli/',
+    build: {
+        assets: 'assets',
+    }
 });
